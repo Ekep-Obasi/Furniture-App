eef6ec476a8d24a50f4832a40933d60b7a6317d6
Ekep Obasi <ekepobasi@gmail.com>
2023-09-27 22:29:00 +0100

    implemented safe loading of fonts using useFont hook

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+// prevents Splash screen from auto hiding while fonts are loaded
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    semiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    extraBold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+  });
+
+  // after custom fonts have been loaded we hide the splash screen and show the app screen
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+
+  if(!fontsLoaded) return null;
 
   return (
     <View style={styles.container}>
@@ -14,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
